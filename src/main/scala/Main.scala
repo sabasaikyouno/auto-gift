091f7ef54a37e576ffe3ab3947bb248383ef9a3c
sabasaikyouno <0852kuro@gmail.com>
2023-11-16 19:02:02 +0900

    ツイッターログイン

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
-object Main {
-  def main() = {
+import java.io.File
 
+import org.openqa.selenium.chrome.ChromeDriver
+import java.time.Duration
+
+import TwitterLogin.login
+import com.github.tototoshi.csv.CSVReader
+
+object Main {
+  def main(args: Array[String]) = {
+    implicit val chrome: ChromeDriver = new ChromeDriver()
+    chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(5))
+
+    val twitterLoginIte = CSVReader.open(new File("src\\main\\resources\\twitter_login.csv")).iterator
+    val twitterId = twitterLoginIte.next().head
+    login(twitterId, twitterLoginIte.next().head)
   }
 }
