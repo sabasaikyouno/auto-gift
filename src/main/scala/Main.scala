e41e017f4faf896da42e85b37a9d2a53ce8807a7
sabasaikyouno <0852kuro@gmail.com>
2023-11-16 19:26:47 +0900

    chromeプロファイル設定

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import java.io.File
 
-import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 import java.time.Duration
 
 import TwitterLogin.login
@@ -8,8 +8,15 @@ import com.github.tototoshi.csv.CSVReader
 
 object Main {
   def main(args: Array[String]) = {
-    implicit val chrome: ChromeDriver = new ChromeDriver()
+    System.setProperty("webdriver.chrome.driver", "chrome\\chromedriver_win32\\chromedriver.exe")
+    val options = new ChromeOptions()
+    options.addArguments("--user-data-dir=\\chrome")
+    options.addArguments("--profile-directory=Profile1")
+
+    implicit val chrome: ChromeDriver = new ChromeDriver(options)
     chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(5))
+
+    chrome.get("https://twitter.com/home")
 
     val twitterLoginIte = CSVReader.open(new File("src\\main\\resources\\twitter_login.csv")).iterator
     val twitterId = twitterLoginIte.next().head
