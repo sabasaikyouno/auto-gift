0a612a569babc8888c86252336068a63c0872b46
sabasaikyouno <0852kuro@gmail.com>
2023-11-20 18:28:15 +0900

    フォローしている人はフォローしないようにする

--- src/main/scala/TwitterActions.scala
+++ src/main/scala/TwitterActions.scala
@@ -20,7 +20,9 @@ object TwitterActions {
     //要素が読み込まれるまで待つ
     chrome.findElement(By.cssSelector(".css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv"))
 
-    chrome.findElement(By.cssSelector(".css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr")).click()
+    Try{
+      chrome.findElement(By.cssSelector(".css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr")).click()
+    }.getOrElse(())
   }
 
   def change_account(id: String)(implicit chrome: ChromeDriver) = {
