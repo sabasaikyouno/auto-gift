3ac9ff6c03f329cb4934d9e3283c31713fcd1ec2
sabasaikyouno <0852kuro@gmail.com>
2023-11-20 18:25:46 +0900

    following書き込み

--- src/main/scala/VariousCSV.scala
+++ src/main/scala/VariousCSV.scala
@@ -2,11 +2,19 @@ import java.io.File
 
 import com.github.tototoshi.csv.{CSVReader, CSVWriter}
 
+import scala.collection.mutable
+
 object VariousCSV {
   def getFollowing = {
     val reader = CSVReader.open(new File("src\\main\\resources\\following.csv"))
 
     reader.readNext.getOrElse(List())
+  }
+
+  def followingWrite(list: mutable.HashSet[String]) = {
+    val writer = CSVWriter.open(new File("src\\main\\resources\\following.csv"))
+
+    writer.writeRow(list.toList)
   }
 
   def getAccounts = {
