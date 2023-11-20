f0101b422c0ac43c2bbb1a7ac86b5c92a857e060
sabasaikyouno <0852kuro@gmail.com>
2023-11-20 17:08:23 +0900

    CSV関連

---
+++ src/main/scala/VariousCSV.scala
@@ -0,0 +1,17 @@
+import java.io.File
+
+import com.github.tototoshi.csv.{CSVReader, CSVWriter}
+
+object VariousCSV {
+  def getFollowing = {
+    val reader = CSVReader.open(new File("src\\main\\resources\\following.csv"))
+
+    reader.all().head
+  }
+
+  def getAccounts = {
+    val reader = CSVReader.open(new File("src\\main\\resources\\twitter_login.csv"))
+
+    reader.all().flatten.grouped(2).map(l => (l.head, l.last)).toList
+  }
+}
